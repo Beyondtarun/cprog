ce320bfd6e37dc16d61b67aa7d62354c1ff9a32d
Beyondtarun <teg.tarun@gmail.com>
2023-12-22 17:47:43 +0530

    update

--- patterns.cpp
+++ patterns.cpp
@@ -9,6 +9,8 @@ void kite(int n);
 void rotatedTriangle(int n);
 void numberCrown(int n);
 void binaryTriangle(int n);
+void symmetryCrown(int n);
+void symmetryCrownVoid(int n);
 
 int main()
 {
@@ -19,7 +21,7 @@ int main()
 
     cout << "\n\n\tEnter the value on Lines: ";
     cin >> n;
-    cout << "\n\tChoice(s)\n\n\t\t1. Right Half Triangle.\n\t\t2. Left Half Triangle.\n\t\t3. Full Pyramid.\n\t\t4. Reverse Pyramid.\n\t\t5. Kite.\n\t\t6. Rotated Triangle.\n\t\t7. Binary Triangle.\n\t\t8. Number Crown.";
+    cout << "\n\tChoice(s)\n\n\t\t1. Right Half Triangle.\n\t\t2. Left Half Triangle.\n\t\t3. Full Pyramid.\n\t\t4. Reverse Pyramid.\n\t\t5. Kite.\n\t\t6. Rotated Triangle.\n\t\t7. Binary Triangle.\n\t\t8. Number Crown.\n\t\t9. Symmetric Crown.\n\t\t10. Symmetric Crown Void.";
     int choice;
     cout << "\n\n\tEnter choice: ";
     cin >> choice;
@@ -45,18 +47,24 @@ int main()
     case 6:
         rotatedTriangle(n);
         break;
-    case 7: 
+    case 7:
         binaryTriangle(n);
         break;
-    case 8: 
+    case 8:
         numberCrown(n);
+        break;
+    case 9:
+        symmetryCrown(n);
+        break;
+    case 10:
+        symmetryCrownVoid(n);
         break;
 
     default:
         cout << "\n\tWrong Choice";
         break;
     }
-    cout << "\n+++++++++++++++++++++++++++++++++++++++++++++\n\n";
+    cout << "+++++++++++++++++++++++++++++++++++++++++++++\n\n";
     return 0;
 }
 void rightHalfTriangle(int n)
@@ -139,7 +147,8 @@ void kite(int n)
         cout << "\n";
     }
 }
-void rotatedTriangle(int n){
+void rotatedTriangle(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -148,36 +157,111 @@ void rotatedTriangle(int n){
         }
         cout << "\n";
     }
-      for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < n-i; j++)
+        for (int j = 0; j < n - i; j++)
         {
             cout << "*";
         }
         cout << "\n";
     }
 }
-void numberCrown(int n){
-     for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++)
-        cout<<j+1<<" ";
-        for(int k=i*2;k<(n*2)-2;k++)
-        cout<<"  ";
-        for(int z=i;z>=0;z--)
-        cout<<z+1<<" ";
-        cout<<"\n";
+void numberCrown(int n)
+{
+    if (n > 9)
+    {
+        cout << "Enter a number less than 10\n\n";
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << j + 1 << " ";
+            for (int k = i * 2; k < (n * 2) - 2; k++)
+                cout << "  ";
+            for (int z = i; z >= 0; z--)
+                cout << z + 1 << " ";
+            cout << "\n";
+        }
     }
 }
-void binaryTriangle(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            if((i+j)%2==0){
-                cout<<"1 ";
+void binaryTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1 ";
             }
-            else{
-                cout<<"0 ";
+            else
+            {
+                cout << "0 ";
             }
         }
-        cout<<"\n";
+        cout << "\n";
+    }
+}
+
+void symmetryCrown(int n)
+{
+    if (n > 9)
+    {
+        cout << "Enter a number less than 10\n\n";
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "* ";
+            for (int k = i * 2; k < (n * 2) - 2; k++)
+                cout << "  ";
+            for (int z = i; z >= 0; z--)
+                cout << "* ";
+            cout << "\n";
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "* ";
+            for (int k = i * 2; k < (n * 2) - 2; k++)
+                cout << "  ";
+            for (int z = i; z >= 0; z--)
+                cout << "* ";
+            cout << "\n";
+        }
+    }
+}
+void symmetryCrownVoid(int n)
+{
+    if (n > 9)
+    {
+        cout << "Enter a number less than 10\n\n";
+    }
+    else
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "* ";
+            for (int k = i * 2; k < (n * 2) - 2; k++)
+                cout << "  ";
+            for (int z = i; z >= 0; z--)
+                cout << "* ";
+            cout << "\n";
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "* ";
+            for (int k = i * 2; k < (n * 2) - 2; k++)
+                cout << "  ";
+            for (int z = i; z >= 0; z--)
+                cout << "* ";
+            cout << "\n";
+        }
     }
 }
