52440a314292b606e79301a08cf2e0daf2043910
Beyondtarun <teg.tarun@gmail.com>
2023-12-23 11:09:26 +0530

    upates

--- C++STL.cpp
+++ C++STL.cpp
@@ -1,59 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 void explainPair();
 void explainVector();
-int main(){
+int main()
+{
 
     // explainPair();
     explainVector();
     return 0;
 }
 
-void explainPair(){
-    pair<int,int> p ={1,2};
-    cout<<p.first<<p.second;
-    pair<int,pair<int,int>> l ={1,{2,3}};
-    cout<<l.first<<l.second.first;
-    pair<int,int> arr[]={{1,2},{2,5},{3,4}};
-    cout<<arr[1].second;
+void explainPair()
+{
+    pair<int, int> p = {1, 2};
+    cout << p.first << p.second;
+    pair<int, pair<int, int>> l = {1, {2, 3}};
+    cout << l.first << l.second.first;
+    pair<int, int> arr[] = {{1, 2}, {2, 5}, {3, 4}};
+    cout << arr[1].second;
 }
-void explainVector(){
-//it is dynamic array
-//indexing start from 0
-vector<int> v;
+void explainVector()
+{
+    // it is dynamic array
+    // indexing start from 0
+    vector<int> v;
 
-//push_back & emplace_back (faster then push_back)
-v.push_back(11);//insert 1 into vector
-v.push_back(12);
-v.push_back(16);
-v.push_back(95);
-v.push_back(45);
-v.push_back(145);
-v.emplace_back(22);//insert 2 into vector
-cout<<v[0]<<v[1]; //return garbage value on non existent index
+    // push_back & emplace_back (faster then push_back)
+    v.push_back(11); // insert 1 into vector
+    v.push_back(12);
+    v.push_back(16);
+    v.push_back(95);
+    v.push_back(45);
+    v.push_back(145);
+    v.emplace_back(22);   // insert 2 into vector
+    cout << v[0] << v[1]; // return garbage value on non existent index
 
-vector<pair<int,int>> p;
-p.push_back({1,2});
-p.emplace_back(1,2);//automatically assume {}
-cout<<"\n"<<p[0].first<<" "<<p[0].second;
+    vector<pair<int, int>> p;
+    p.push_back({1, 2});
+    p.emplace_back(1, 2); // automatically assume {}
+    cout << "\n"
+         << p[0].first << " " << p[0].second;
 
-vector<int> a(5,100); // a={100,100,100,100,100}
-vector<int> b(5);//b={0,0,0,0,0} ; depends on compiler
+    vector<int> a(5, 100); // a={100,100,100,100,100}
+    vector<int> b(5);      // b={0,0,0,0,0} ; depends on compiler
 
+    vector<int> c(a); // copy vector
 
-vector<int> c(a);//copy vector
+    // ITERATORS
 
-//ITERATORS
+    vector<int>::iterator it = v.begin(); // points to the memrory of first index
+    // it =it+2//shifts 2 index ahead;
+    // vector<int>:: iterator it = v.end();//point to the null element or one ahead the last element we need to do it-- to point it on last element of the vector;
+    // vector<int>:: iterator it = v.rend();//point before the zero index
+    // vector<int>:: iterator it = v.rbegin(); //point to the last element
+    cout << "\n"
+         << *it;
+    cout << "\n"
+         << v.back(); // print last element
+    cout << "\n Loop Begins";
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << "\n"
+             << *it;
+    }
+    
+//auto: auto assign the data type
+    for(auto it : v){ // this is not a iterator
+        cout<<"\n"<<it;
+    }
+    for(auto it = v.begin();it!=v.end();it++){
+        cout<<"\n"<<*it;
+    }
 
-vector<int>:: iterator it = v.begin();//points to the memrory of first index
-//it =it+2//shifts 2 index ahead;
-//vector<int>:: iterator it = v.end();//point to the null element or one ahead the last element we need to do it-- to point it on last element of the vector;
-//vector<int>:: iterator it = v.rend();//point before the zero index
-//vector<int>:: iterator it = v.rbegin(); //point to the last element
-cout<<"\n"<<*it;
-cout<<"\n"<<v.back();//print last element
-cout<<"\n Loop Begins";
-for(it=v.begin();it!=v.end();it++){
-    cout<<"\n"<<*it;
-}
+//Deletion in a vector
+
 }
